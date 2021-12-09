ce83515135cf67aa0adab7f2981f3a33ba5a21af
crkobel <crkobel@verizon.net>
2021-12-09 09:45:23 -0800

    for loop to print numbers, fizz, buzz, and fizzbuzz

--- ConnorAssignments/FizzBuzz/Program.cs
+++ ConnorAssignments/FizzBuzz/Program.cs
@@ -27,8 +27,22 @@ do {
     Console.WriteLine("Enter a positive number");
     input = Console.ReadLine();
     number = int.Parse(input);
+    Console.WriteLine();
 
     if (number <= 0) {
         Console.WriteLine("The number should be greater than 0");
     }
 } while (number <= 0);
+
+// count up to the number and print all numbers
+for (int i = 1; i <= number; i++) {
+    if (i % 15 == 0) {
+        Console.WriteLine("FizzBuzz (" + i + ")");
+    } else if (i % 3 == 0) {
+        Console.WriteLine("Fizz (" + i + ")");
+    } else if (i % 5 == 0) {
+        Console.WriteLine("Buzz (" + i + ")");
+    } else {
+        Console.WriteLine(i);
+    }
+}
