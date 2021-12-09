998f50009b1f87ac9ad51b683b676d8c12458d6f
crkobel <crkobel@verizon.net>
2021-12-09 09:34:19 -0800

    updated fizzbuzz program

--- ConnorAssignments/FizzBuzz/Program.cs
+++ ConnorAssignments/FizzBuzz/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine();
+Console.WriteLine("FizzBuzz");
+
+// Console.WriteLine("Enter a positive integer");
+// // string variable for input
+// String input = Console.ReadLine();
+
+// // verifying input
+// Console.WriteLine("You entered " + input);
+
+// // change the string to an integer
+// int number = int.Parse(input);
+// Console.WriteLine("We parsed your input to " + number);
+
+// while (number <= 0) {
+//     Console.WriteLine("The number should be greater than 0");
+//     Console.WriteLine("Enter a positive number");
+//     input = Console.ReadLine();
+//     number = int.Parse(input);
+//     Console.WriteLine();
+// }
+
+
+string input = "";
+int number = 0;
+do {
+    Console.WriteLine("Enter a positive number");
+    input = Console.ReadLine();
+    number = int.Parse(input);
+
+    if (number <= 0) {
+        Console.WriteLine("The number should be greater than 0");
+    }
+} while (number <= 0);
