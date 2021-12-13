8b0131b14f96c83dc7197be7b4d87a3da4dcf445
crkobel <crkobel@verizon.net>
2021-12-13 09:53:15 -0800

    Stopping point before lunch

--- ConnorAssignments/RestaurantReview_Me/Program.cs
+++ ConnorAssignments/RestaurantReview_Me/Program.cs
@@ -4,5 +4,7 @@ Console.WriteLine("Welcome to Restaurant Review!");
 
 Restaurant newResto = new Restaurant();
 string name = newResto.Name = "Carl's Jr.";
+string city = newResto.City = "Huntington Beach";
+string state = newResto.State = "CA";
 
-Console.WriteLine(name);
+Console.WriteLine($"Restaurant:\t{name} \nCity:\t\t{city} \nState:\t\t{state}");
--- ConnorAssignments/RestaurantReview_Me/Restaurant.cs
+++ ConnorAssignments/RestaurantReview_Me/Restaurant.cs
@@ -9,6 +9,20 @@ public class Restaurant {
     // Public is the most visible, private is the least visible
     // By default, class member has access modifier set to private
     // Class, by default, are internal unless you explicitely say otherwise
+
     public string Name { get; set; }
-    
+
+    // private string _name;
+
+    // our own custom getter and setter for the private backing field
+    // // private string GetName() {
+    //     Restaurant this._name;
+    // }
+
+    // private void SetName(string name) {
+    //     this._name = name;
+    // }
+
+    public string City { get; set; }
+    public string State { get; set; }
 }
